5c884e215a54d0e9aa5bc83b2e0dcbd841f97547
ekimaycicek <ekimaycicek@icloud.com>
2023-09-18 19:59:33 +0300

    new

--- src/test/java/Circle.java
+++ src/test/java/Circle.java
@@ -24,6 +24,7 @@ public class Circle extends Shape {
         return this.halfDiameter*Pi*this.halfDiameter;
     }
 
+
     @Override
     public double environmentCalculate() {
         return 2*Pi*this.halfDiameter;
